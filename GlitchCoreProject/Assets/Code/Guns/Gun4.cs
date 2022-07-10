e5ae2e8c20cf55a279283892eb746d6d3ab74ffb
s <samu.lehtineva@gmail.com>
2022-07-10 15:21:51 +0300

    working on sword

--- GlitchCoreProject/Assets/Code/Guns/Gun4.cs
+++ GlitchCoreProject/Assets/Code/Guns/Gun4.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GC.GlitchCoreProject
 {
     public class Gun4 : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public Transform orientation;
+        public float fireDelay;
+        public InputManager inputManager;
+
+        private PlayerInput playerInput;
+        private float fireTimer = 0.0f;
+
+		private void FixedUpdate()
+		{
+			if (fireTimer < fireDelay)
+			{
+				fireTimer += 1.0f;
+			}
+		}
+
+        void Fire1 (InputAction.CallbackContext context)
+		{
+            if (fireTimer >= fireDelay)
+			{
+
+			}
+		}
+
+        private void OnEnable()
         {
-        
+            playerInput = inputManager.playerInput;
+
+            playerInput.Player.Fire1.Enable();
+            playerInput.Player.Fire1.performed += Fire1;
+            fireTimer = fireDelay * 0.75f;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnDisable()
         {
-        
+            playerInput.Player.Fire1.Disable();
+            playerInput.Player.Fire1.performed -= Fire1;
         }
     }
 }
