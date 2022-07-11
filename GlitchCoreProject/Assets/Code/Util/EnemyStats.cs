f0d519b05e89672284d409f8ce9236394ad3c876
s <samu.lehtineva@gmail.com>
2022-07-11 19:20:13 +0300

    Enemy death stuff

--- GlitchCoreProject/Assets/Code/Util/EnemyStats.cs
+++ GlitchCoreProject/Assets/Code/Util/EnemyStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GC.GlitchCoreProject
 {
@@ -9,8 +10,18 @@ namespace GC.GlitchCoreProject
         public int maxHealth;
         public int currentHealth;
 
+        private SkinnedMeshRenderer render;
+        private Animator anim;
+
 		private void Awake()
 		{
+            anim = GetComponentInChildren<Animator>();
+            render = GetComponentInChildren<SkinnedMeshRenderer>();
+            for (int i = 0; i < render.materials.Length; i++)
+			{
+                render.materials[i] = new Material(render.materials[i]);
+			}
+            Debug.Log(render.materials[0].shader);
             currentHealth = maxHealth;
 		}
 
@@ -26,7 +37,16 @@ namespace GC.GlitchCoreProject
 
         public void Die()
         {
-            Destroy(this.gameObject);
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<BoxEnemyController>().enabled = false;
+            GetComponent<NavMeshAgent>().enabled = false;
+            anim.enabled = false;
+
+            foreach (Material mat in render.materials)
+			{
+                mat.shader = Shader.Find("Shader Graphs/Diffuse");
+			}
+            Destroy(gameObject, 5);
         }
     }
 }
