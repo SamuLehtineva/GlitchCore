f5adae3a4d22d3307994919c04e53fa5aa57783c
s <samu.lehtineva@gmail.com>
2022-07-18 16:53:27 +0300

    Enemy bullet

---
+++ GlitchCoreProject/Assets/Code/Bullets/EnemyBullet_1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GC.GlitchCoreProject
+{
+    public class EnemyBullet_1 : MonoBehaviour
+    {
+        public float speed;
+        public float lifeTime;
+
+        float timer = 0.0f;
+        int layerMask = 12;
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            transform.position += transform.forward * speed * Time.deltaTime;
+            CheckHit();
+
+            if (timer > lifeTime)
+			{
+                Destroy(gameObject);
+			}
+        }
+
+        void CheckHit()
+		{
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 0.2f, ~layerMask))
+			{
+                EffectManager.instance.SpawnEffect(0, transform.position, transform.rotation);
+                PlayerStats stats = hit.collider.gameObject.GetComponent<PlayerStats>();
+                if (stats != null)
+				{
+                    stats.Damage(25);
+                    hit.rigidbody.AddForce(transform.forward * 500f);
+				}
+                else if (hit.collider.gameObject.layer == 6)
+				{
+                    DecalManager.instance.CreateDecal(hit, 0);
+				}
+                Destroy(gameObject);
+            }
+		}
+    }
+}
