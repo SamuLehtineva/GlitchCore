2e97323417b69239fa8f535ea20095a1f9c7e688
s <samu.lehtineva@gmail.com>
2022-07-21 15:26:04 +0300

    1st shop item mockup

---
+++ GlitchCoreProject/Assets/Code/Util/RotateTransform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GC.GlitchCoreProject
+{
+    public class RotateTransform : MonoBehaviour
+    {
+        public float speed;
+
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(0f, speed * Time.deltaTime, 0f, Space.Self);
+        }
+    }
+}
